d076679c8a844e30212db164968d87d40c295a5c
Pedro Cambra <pedro@cambrico.net>
2022-11-01 17:22:06 +0100

    Moved structure to a composer based workflow

--- modules/gin_login/README.txt
+++
@@ -1,1 +0,0 @@
-Custom Drupal Login for Gin theme
--- modules/gin_login/webpack.config.build.js
+++
@@ -1,8 +0,0 @@
-// const path = require('path');
-const merge = require('webpack-merge');
-const webpackConfig = require('./webpack.config');
-
-module.exports = merge(webpackConfig, {
-  mode: 'production',
-  devtool: ''
-});
